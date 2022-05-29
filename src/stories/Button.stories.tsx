1dfc1bc3976889ccc44e5587c085461bde85deaf
Kekelberi <alexei.gaponov@gmail.com>
2022-05-29 17:55:54 +0300

    save

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -4,27 +4,15 @@ import { ComponentStory, ComponentMeta } from '@storybook/react'
 export default {
   title: 'Button',
   component: Button,
-  parameters: {
-    zeplinLink:
-      'zpl://components?pid=62927166afb23d1511bff773&coid=6292716a1a8dbc1219fc647f',
-  },
+  parameters: {},
 }
 
 const Template: ComponentStory<typeof Button> = (args) => (
   <Button {...args}>{args.label}</Button>
 )
 
-export const MyButton = Template.bind({
-  zeplinLink:
-    'zpl://components?pid=62927166afb23d1511bff773&coid=6292716a1a8dbc1219fc647f',
-})
+export const MyButton = Template.bind({})
 MyButton.args = {
   label: 'Primary',
   backgroundColor: 'red',
-  zeplinLink:
-    'zpl://components?pid=62927166afb23d1511bff773&coid=6292716a1a8dbc1219fc647f',
-}
-MyButton.parameters = {
-  zeplinLink:
-    'zpl://components?pid=62927166afb23d1511bff773&coid=6292716a1a8dbc1219fc647f',
 }
